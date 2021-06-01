70d1312ab970dc4d1fd439c5f3ecacc238ffac22
Sabyasachi Mazumder <sabyasachi2712@gmail.com>
2021-06-01 19:29:14 +0530

    forth commit

--- Authentication_and_login/register-acc-teacher.php
+++ Authentication_and_login/register-acc-teacher.php
@@ -2,5 +2,5 @@
 
 <?php
     $qry = "INSERT INTO `student` VALUES ('', '".$_REQUEST['name']."', '".$_REQUEST['email']."', '".$_REQUEST['password']."', '".$_REQUEST['mobno']."')";
-
+    $qry_exec = mysqli_query($con, $qry);
 ?>
