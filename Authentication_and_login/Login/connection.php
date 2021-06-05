bac9c403d593d42a436f881ee28345787cacf5db
Sabyasachi Mazumder <sabyasachi2712@gmail.com>
2021-06-05 11:54:22 +0530

    first commit

--- Authentication_and_login/Login/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        $host = "freedb.tech";
-        $username = "freedbtech_savior";
-        $password = "Sm227121999";
-        $databasename = "freedbtech_elearningportal";
-
-        $con = mysqli_connect($host, $username, $password, $databasename);
-?>
