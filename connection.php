2da54db9ca7765862789c861bd26acacb8b6bde6
CrackerCrypto <rajshybal10@gmail.com>
2021-07-19 12:47:31 +0530

    retry

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "sql6.freemysqlhosting.net";
-$username = "sql6424262";
-$password = "vrjfalRvxI";
-$databasename = "sql6424262";
+$host = "sql210.epizy.com";
+$username = "epiz_29173953";
+$password = "HNAmiOY5d9x";
+$databasename = "epiz_29173953_StudyLand";
 
 $con = mysqli_connect($host, $username, $password, $databasename);
 ?>
