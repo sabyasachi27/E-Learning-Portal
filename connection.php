930bcee3c8d67ee9ea595d288795a147906f27f8
subhrajit2000 <nayaksubhrajit84@gmail.com>
2021-07-23 10:50:21 +0530

    some changes

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$databasename = "StudyLand";
+$databasename = "studyland";
 
 $con = mysqli_connect($host, $username, $password, $databasename);
 ?>
