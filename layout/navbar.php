795f0cd831595269fdad1c10ee6068ffce86c66d
subhrajit2000 <nayaksubhrajit84@gmail.com>
2021-07-24 11:30:41 +0530

    changes in the navbar

--- layout/navbar.php
+++ layout/navbar.php
@@ -19,8 +19,8 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
+            <li><a href="#popular-courses">Popular courses</a></li>
+            <li><a href="#contact">contact</a></li>
             <li><a href="#">Something else here</a></li>
             <li class="divider"></li>
             <li><a href="#">Separated link</a></li>
