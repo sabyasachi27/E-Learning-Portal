acf71c390d11635135c4ccd48e24f9c11cba17d0
Sabyasachi Mazumder <sabyasachi2712@gmail.com>
2021-07-27 02:12:50 +0530

    update contents

--- contents/update-contents-acc.php
+++ contents/update-contents-acc.php
@@ -3,14 +3,14 @@
 <?php
 
 	// running a query
-	$qry = "UPDATE `contents` SET `heading` = '".$_REQUEST['heading']."', `type` = '".$_REQUEST['type']."', `links` = '".$_REQUEST['links']."', `content`= '".$_REQUEST['content']."', `updatedAt`='".date("Y-m-d H:i:s")."' WHERE `id` = ".$_REQUEST['id'];
+	$qry = "UPDATE `contents` SET `contentname` = '".$_REQUEST['cont-name']."', `type` = '".$_REQUEST['type']."', `links` = '".$_REQUEST['link']."', `content`= '".$_REQUEST['content']."', `updatedAt`='".date("Y-m-d H:i:s")."' WHERE `id` = ".$_REQUEST['id'];
 	$qry_exec = mysqli_query($con, $qry);
-	$qry1= mysqli_query($con, "SELECT * FROM `contents` WHERE `id` = ".$_REQUEST['id']);
+	$qry1= mysqli_query($con, "SELECT * FROM `contents` WHERE `id` = '".$_REQUEST['id']."'");
     $row = mysqli_fetch_array($qry1);
     //print_r($row['courseid']);die();
 	if ($qry_exec) {
 		echo "Update successfully";
-		header("Location: contents.php?id=".$row['courseid']);
+		header("Location: contents.php");
 	
 	} else {
 		echo "Failed";
